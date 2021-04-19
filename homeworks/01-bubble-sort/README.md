95f8dfd4448e77cbb1a6845da127ddbd2539b3be
Victor Kostyuk <torcoste@gmail.com>
2021-04-19 18:35:25 +0300

    [hw-01] delete for create PR

--- homeworks/01-bubble-sort/README.md
+++
@@ -1,5 +0,0 @@
-# Bubble Sort Visualization
-
-Demo: http://bubble-sort-visualization.surge.sh
-
-[Task description](task.md)
--- homeworks/01-bubble-sort/src/model/color.ts
+++
@@ -1,2 +0,0 @@
-export const getRainbowColor = (index: number, length: number) =>
-  `hsl(${(360 * index) / length}, 100%, 50%)`
